be20c00c64db8948481cd709ccc8e8210af6d216
Matt Farmer <matt@frmr.me>
2018-11-04 15:13:49 -0500

    Add config,logback

--- src/main/java/me/frmr/kafkahawk/Main.java
+++ src/main/java/me/frmr/kafkahawk/Main.java
@@ -1,7 +1,16 @@
 package me.frmr.kafkahawk;
 
+import com.typesafe.config.ConfigFactory;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class Main {
+  private static Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    logger.info("Kafka Hawk Booting.");
+
+    var config = ConfigFactory.load();
+    logger.info("Config loaded.");
   }
 }
