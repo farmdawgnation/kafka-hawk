77a4386a2d139c59c27e0752104c132d30cdb2fd
Matt Farmer <matt@frmr.me>
2018-11-04 15:20:45 -0500

    Get initial prometheus http output up and running

--- src/main/java/me/frmr/kafkahawk/Main.java
+++ src/main/java/me/frmr/kafkahawk/Main.java
@@ -1,6 +1,9 @@
 package me.frmr.kafkahawk;
 
 import com.typesafe.config.ConfigFactory;
+import java.io.IOException;
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -12,5 +15,17 @@ public class Main {
 
     var config = ConfigFactory.load();
     logger.info("Config loaded.");
+
+    DefaultExports.initialize();
+    logger.info("Hotspot metrics collectors started.");
+
+    try {
+      var server = new HTTPServer(config.getInt("hawk.prometheus.port"), false);
+    } catch (IOException ioe) {
+      logger.error("HTTPServer failed to start", ioe);
+      System.exit(255);
+    }
+
+    logger.info("Prometheus HTTP server started.");
   }
 }
