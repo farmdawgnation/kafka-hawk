de2fc8613210bbb365df6935f53fe0ccbe7dba3f
Matt Farmer <matt@frmr.me>
2018-11-04 16:45:13 -0500

    Add an initial, failing test

---
+++ src/test/java/me/frmr/kafkahawk/HawkConsumerTest.java
@@ -0,0 +1,20 @@
+package me.frmr.kafkahawk;
+
+import kafka.coordinator.group.*;
+
+import org.junit.jupiter.api.*;
+
+public class HawkConsumerTest {
+  @Test
+  void consumeSingleRecordTest() {
+    // Get access to GroupMetadataManager.offsetCommitKey through
+    // reflection hackery
+
+    //Create an offset key and serialize it
+
+    // Run it through the consume method
+
+    // Check the counter
+    throw new RuntimeException("boo");
+  }
+}
