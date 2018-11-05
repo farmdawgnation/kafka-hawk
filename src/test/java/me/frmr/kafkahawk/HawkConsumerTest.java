87f4870d4f1e522505776f26a373dfd9428852b0
Matt Farmer <matt@frmr.me>
2018-11-04 20:27:40 -0500

    Add a basic test to cover that it works

--- src/test/java/me/frmr/kafkahawk/HawkConsumerTest.java
+++ src/test/java/me/frmr/kafkahawk/HawkConsumerTest.java
@@ -1,11 +1,26 @@
 package me.frmr.kafkahawk;
 
+import java.io.IOException;
 import kafka.coordinator.group.*;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HawkConsumerTest {
   @Test
-  void consumeSingleRecordTest() throws NoSuchMethodException {
+  void consumeSingleRecordTest() throws NoSuchMethodException, IOException {
+    var key = getClass().getClassLoader().getResourceAsStream("key_0.dat").readAllBytes();
+    var value = getClass().getClassLoader().getResourceAsStream("msg_0.dat").readAllBytes();
+
+    var record = new ConsumerRecord<byte[], byte[]>("__consumer_offsets", 0, 0l, key, value);
+
+    var sut = new HawkConsumer(null, false, null);
+
+    sut.consumeSingleRecord(record);
+
+    assertEquals(1.0, HawkConsumer.commitsCounter.labels("console-consumer-45944", "test").get());
   }
 }
